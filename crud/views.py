851149245fc204c90f47a334ea953647d21a54f9
Pawan Kumar  Shrestha <thisispawann@gmail.com>
2023-05-13 14:30:42 +0545

    navigate to and created register form

--- crud/views.py
+++ crud/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from django.contrib.auth.forms import UserCreationForm
+
 # Create your views here.
 def index(request):
     return render(request, "index.html")
+
+def register(request):
+    f = UserCreationForm()
+    return render(request, "registration/register.html", {'form': f})
